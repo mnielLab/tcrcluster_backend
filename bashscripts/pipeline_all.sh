cfbd554f842c9d830632c54186e99985b53bbcad
Yat-tsai Richie Wan <richieytwan@yahoo.fr>
2025-01-03 12:13:25 +0100

    updated import paths

--- bashscripts/pipeline_all.sh
+++ bashscripts/pipeline_all.sh
@@ -121,11 +121,12 @@ mkdir -p /tmp/${JOBID} # ??
 cd ${SRCDIR}
 # TODO : check the arguments and change everything accordingly
 chmod 755 "/home/locals/tools/src/TCRcluster-1.0/src/"
+chmod 755 $SRCDIR
 # Call the Python script with the correctly set threshold
-#PYTHON="/home/ctools/opt/anaconda3_202105/bin/python3"
-PYTHON=/home/people/riwa/anaconda3/envs/cuda/bin/python3.11
+PYTHON="/home/ctools/opt/anaconda3_202105/bin/python3"
+#PYTHON=/home/people/riwa/anaconda3/envs/cuda/bin/python3.11
 # todo: DEBUG with -np 10, njob 5 ; when done, remove
-echo "Starting python script with"
+echo "Starting python script in $(pwd)"
 # Debugging (Optional: Print variables to check values)
 echo "PYTHON: $PYTHON"
 echo "JOBID: $JOBID"
--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -1,10 +1,16 @@
 import argparse
 from tqdm.auto import tqdm
 import os, sys
-
-module_path = os.path.abspath(os.path.join('..'))
+module_path = os.path.abspath(os.path.join('../..'))
 if module_path not in sys.path:
     sys.path.append(module_path)
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/'
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+# Get the absolute path of the "src" directory and HARDCODEDDLY ADDING IT
+src_path = '/home/local/tools/src/TCRcluster-1.0/src/'
+# Add the "src" directory to the Python module search path
+sys.path.append(src_path)
 import pandas as pd
 import seaborn as sns
 from cluster_utils import *
